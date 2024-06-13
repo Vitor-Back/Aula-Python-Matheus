f18024361f80867be4da2c3821525c0c30d6f245
Vitor Back <vitor.back10@outlook.com>
2024-06-13 19:54:29 -0400

    mensagem com format

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ valor = 500.00
 desc = '10'
 mes = 'janeiro'
 cup = 'PAULAÉ10'
+mensagem = f"O nome da pessoa é {nome} ela tem um desconto de {desc}% e pode usar o cupom {cup} por conta da compra em {mes}"
 
-print("Olá,", nome, "Em", mes, "você realizou uma compra no valor de R$", valor, "e ganhou um desconto de", desc + "%", "em sua próxima compra. Use o cupom", cup)
+print(mensagem)
+
 
