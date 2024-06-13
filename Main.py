d99e16007a0ae71ca7f4624dc2f8cb32202329fc
Vitor Back <vitor.back10@outlook.com>
2024-06-13 19:38:34 -0400

    Aula de python do matheus

---
+++ Main.py
@@ -0,0 +1,8 @@
+nome = 'PAULA MARTINS'
+valor = 500.00
+desc = '10'
+mes = 'janeiro'
+cup = 'PAULAÉ10'
+
+print("Olá,", nome, "Em", mes, "você realizou uma compra no valor de R$", valor, "e ganhou um desconto de", desc + "%", "em sua próxima compra. Use o cupom", cup)
+
